fa1672dce36593aac2cc379b62e9d96e3fb19349
salekin02 <36966165+salekin02@users.noreply.github.com>
2025-10-19 11:24:41 +0600

    deploy example

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+// import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'parallel-router': path.resolve(__dirname, '../src/index.ts'),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     'parallel-router': path.resolve(__dirname, '../src/index.ts'),
+  //   },
+  // },
+  base: './',
   server: {
     port: 3002,
     open: true,
